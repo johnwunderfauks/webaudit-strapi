e5e3688c62ec8026d0f7f941881710ce227eaaf3
minlapyae <122955892+minlapyae@users.noreply.github.com>
2023-02-15 11:31:23 +0630

    Create a new collection called Page

--- config/database.js
+++ config/database.js
@@ -2,6 +2,7 @@ const path = require('path');
 const parse = require('pg-connection-string').parse;
 const config = parse(process.env.DATABASE_URL);
 
+
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
